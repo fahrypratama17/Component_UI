dcf12f25759ab2c185d98237665429fa39bc03ea
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2025-08-26 16:13:55 +0700

    add JavaScript functionality for Animated Progress Bar

---
+++ Animated_Progress_Bar/script.js
@@ -0,0 +1,17 @@
+let progress = 0;
+
+const progressCircle = document.querySelector('.circle-progress');
+const percentageText = document.querySelector('.percentage');
+const statusText = document.querySelector('.status');
+
+const interval = setInterval(() => {
+    if (progress < 100) {
+        progress += 1;
+        const offset = 339.292 - (339.292 * progress) / 100;
+        progressCircle.style.strokeDashoffset = offset;
+        percentageText.textContent = `${progress}`;
+    } else {
+        clearInterval(interval);
+        statusText.textContent = 'Download Completed'
+    }
+}, 60);
